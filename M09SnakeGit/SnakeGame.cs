b208a68d7b69ec47eded76439cc2fb6c48359835
Pol Ferdman <a2006407@institutmontilivi.cat>
2021-11-29 17:31:02 +0100

    v0.4 amb el projecte be

---
+++ M09SnakeGit/SnakeGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M09SnakeGit {
+
+    public enum DireccioSnake {
+        Dreta,Esquerra,Avall,Amunt
+    }
+
+    class SnakeGame {
+
+        public const int X_SIZE = 5;
+        public const int Y_SIZE = 5;
+        private Point capSerp = new Point(0,0);
+
+        public Point CapSerp { get => capSerp; set => CapSerp = value; }
+
+
+        internal void moure() {
+            capSerp.Y++;
+        }
+
+
+    }
+}
